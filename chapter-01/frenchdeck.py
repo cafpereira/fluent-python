69680fba0cc913d12cb603b3551ca17e8fdda235
cpereira <cpereira@groupon.com>
2018-05-09 22:57:33 -0400

    add comments

--- chapter-01/frenchdeck.py
+++ chapter-01/frenchdeck.py
@@ -10,6 +10,9 @@ class FrenchDeck:
         self._cards = [Card(rank, suit) for suit in self.suits
                                         for rank in self.ranks]
 
+    # By implementing the special methods __len__ and __getitem__, deck behaves like a standard Python
+    # sequence, allowing it to benefit from core language features (e.g., iteration and slicing)
+
     def __len__(self):
         return len(self._cards)
 
@@ -29,11 +32,19 @@ choice(deck)
 for d in deck:
     print(d)
 
-Card('Q', 'hearts') in deck
+print(Card('Q', 'hearts') in deck)
+# >>> True
 
+print(Card('7', 'foo') in deck)
+# >>> False
+
+# Sorting
+
+# Rank cards by suit (spades is the highest)
 suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 
 def spades_high(card):
+    # card rank uses the index position (ace is the highest)
     rank_value = FrenchDeck.ranks.index(card.rank)
     return rank_value * len(suit_values) + suit_values[card.suit]
 
